f78bbe17b43c7c840fda1d45e26dcbdae860d895
Philippe Marschall <philippe.marschall@gmail.com>
2012-12-30 11:54:45 +0100

    add test

--- src/main/java/com/github/marschall/filexor/Main.java
+++ src/main/java/com/github/marschall/filexor/Main.java
@@ -1,5 +1,7 @@
 package com.github.marschall.filexor;
 
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,42 +9,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static java.nio.file.StandardOpenOption.CREATE_NEW;
-import static java.nio.file.StandardOpenOption.READ;
-import static java.nio.file.StandardOpenOption.WRITE;
-
 
 public class Main {
 
-  
+
   private static final int BUFFER_SIZE = 0x2000;
-  
+
   public static void main(String[] args) throws IOException {
     if (args.length != 1) {
-      System.out.println("usage: file-xor.javar file");
+      System.out.println("usage: java -jar file-xor.jar file");
       System.exit(1);
     }
     Path path = Paths.get(args[0]);
     xor(path);
   }
-  
+
   static void xor(Path path) throws IOException {
     String fileName = path.getFileName().toString();
-    
+
     Path target;
     if (fileName.endsWith(".xor")) {
       target = path.resolveSibling(fileName.substring(0, fileName.length() - ".xor".length()));
     } else {
       target = path.resolveSibling(fileName + ".xor");
-    } 
-    
+    }
+
     try (
-        InputStream input = Files.newInputStream(path, READ);
-        OutputStream output = Files.newOutputStream(target, WRITE, CREATE_NEW)) {
+        InputStream input = Files.newInputStream(path);
+        OutputStream output = Files.newOutputStream(target, CREATE_NEW)) {
       xor(input, output);
     }
   }
-  
+
   static void xor(InputStream input, OutputStream output) throws IOException {
     byte[] buffer = new byte[BUFFER_SIZE];
     int read;
@@ -51,11 +49,11 @@ public class Main {
       output.write(buffer, 0, read);
     }
   }
-  
+
   static void xor(byte[] buffer, int len) {
     for (int i = 0; i < len; ++i) {
       byte value = buffer[i];
-      buffer[i] = (byte) (value ^  0);
+      buffer[i] = (byte) (value ^ 255);
     }
   }
 
--- src/test/java/com/github/marschall/filexor/MainTest.java
+++ src/test/java/com/github/marschall/filexor/MainTest.java
@@ -1,19 +1,67 @@
 package com.github.marschall.filexor;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 import org.junit.Rule;
 import org.junit.Test;
 
 public class MainTest {
-	
-	@Rule
-	public final FileSystemRule rule = new FileSystemRule();
 
-	@Test
-	public void xor() {
-	}
-	
-	@Test
-	public void deXor() {
-	}
+  @Rule
+  public final FileSystemRule rule = new FileSystemRule();
+
+  @Test
+  public void xor() throws IOException {
+    // given
+    FileSystem fileSystem = rule.getFileSystem();
+    Path input = fileSystem.getPath("file.txt");
+    Path output = input.resolveSibling("file.txt.xor");
+    byte[] data = new byte[]{0b00000000, 0b01010101, (byte) 0b10101010, (byte) 0b11111111};
+    byte[] dataXor = new byte[]{(byte) 0b11111111, (byte) 0b10101010, 0b01010101, (byte) 0b00000000};
+    try (OutputStream stream = Files.newOutputStream(input, StandardOpenOption.CREATE_NEW)) {
+      stream.write(data);
+    }
+    
+    // when
+    Main.xor(input);
+    
+    // then
+    assertTrue(Files.exists(input));
+    assertTrue(Files.exists(output));
+    
+    assertArrayEquals(data, Files.readAllBytes(input));
+    assertArrayEquals(dataXor, Files.readAllBytes(output));
+  }
+
+  @Test
+  public void deXor() throws IOException {
+    // given
+    FileSystem fileSystem = rule.getFileSystem();
+    Path input = fileSystem.getPath("file.txt");
+    Path output = input.resolveSibling("file.txt.xor");
+    byte[] data = new byte[]{0b00000000, 0b01010101, (byte) 0b10101010, (byte) 0b11111111};
+    byte[] dataXor = new byte[]{(byte) 0b11111111, (byte) 0b10101010, 0b01010101, (byte) 0b00000000};
+    try (OutputStream stream = Files.newOutputStream(output, StandardOpenOption.CREATE_NEW)) {
+      stream.write(dataXor);
+    }
+    
+    // when
+    Main.xor(output);
+    
+    // then
+    assertTrue(Files.exists(input));
+    assertTrue(Files.exists(output));
+    
+    assertArrayEquals(data, Files.readAllBytes(input));
+    assertArrayEquals(dataXor, Files.readAllBytes(output));
+  }
 
 }
